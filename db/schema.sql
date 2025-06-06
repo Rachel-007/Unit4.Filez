eba615e2a2f1c943b6d92f98a5fa5fcd6372cf97
Rachel Davis <rachelddavis@aol.com>
2025-06-05 18:36:41 -0600

    Unit4.Filez Updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
 -- TODO
+DROP TABLE IF EXISTS folders CASCADE;
+DROP TABLE IF EXISTS files;
+
+
+CREATE TABLE folders (
+  id serial PRIMARY KEY,
+  name text UNIQUE NOT NULL
+);
+
+create table files (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  size integer NOT NULL,
+  folder_id integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
+  UNIQUE (name, folder_id)
+);
+
+-- Ref: folders.id < files.folder_id
